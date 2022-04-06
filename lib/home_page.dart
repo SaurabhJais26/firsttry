d1acf04824ff599aefbf1d1685081edba9e9e7db
Saurabh Jaiswal <sjsaurabhjais@gmail.com>
2022-04-07 00:20:05 +0530

    Second try

---
+++ lib/home_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  HomePage({Key? key}) : super(key: key);
+  String name = "Jais";
+  final int days = 30;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Catalog App"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text(
+            "Welcome to $days days of flutter by $name ",
+          ),
+        ),
+      ),
+    );
+  }
+}
